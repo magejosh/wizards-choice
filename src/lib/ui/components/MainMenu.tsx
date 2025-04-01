c80bb919b772cb07b3f5ae6a5470802af928b63e
magejosh <magejoshplays@gmail.com>
2025-04-01 13:24:26 -0500

    Update MainMenu.tsx

--- src/lib/ui/components/MainMenu.tsx
+++ src/lib/ui/components/MainMenu.tsx
@@ -363,6 +363,28 @@ const MainMenu: React.FC<MainMenuProps> = ({
           </div>
         </div>
       )}
+
+      <a 
+        target="_blank" 
+        href="https://jam.pieter.com" 
+        style={{ 
+          fontFamily: 'system-ui, sans-serif',
+          position: 'fixed',
+          bottom: '-1px',
+          right: '-1px',
+          padding: '7px',
+          fontSize: '14px',
+          fontWeight: 'bold',
+          background: '#fff',
+          color: '#000',
+          textDecoration: 'none',
+          zIndex: 10000,
+          borderTopLeftRadius: '12px',
+          border: '1px solid #fff'
+        }}
+      >
+        🕹️ Vibe Jam 2025
+      </a>
     </div>
   );
 };

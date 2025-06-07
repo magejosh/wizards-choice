eb8445c96ce65986f3ce7e91710ca9186cb6b0e4
magejosh <magejoshplays@gmail.com>
2025-06-06 20:13:27 -0500

    Merge PR #8: enhance hex grid

--- src/components/battle/index.ts
+++ src/components/battle/index.ts
@@ -6,3 +6,4 @@ export { default as BattleLog } from './BattleLog';
 export { default as PlayerHand } from './PlayerHand';
 export { default as WizardStats } from './WizardStats';
 export { default as HexGrid } from './HexGrid';
+export { TILE_COLORS } from './HexGrid';
